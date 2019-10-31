4242f945c9a6dd5fdc75ed82fe4ad3549fbaf723
Agung Sepruloh <agungsepruloh1996@gmail.com>
2019-10-31 20:20:06 +0700

    Components and Passing multiple props/value

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <CommentDetail
+                author="Jimmy"
+                time="Today at 16:00PM"
+                comments="Nice Tutorial!"
+            />
+            <CommentDetail
+                author="Natali"
+                time="Today at 18:00PM"
+                comments="Nice Post Bro."
+            />
+            <CommentDetail
+                author="Erick"
+                time="Today at 19:00PM"
+                comments="How is it possible?"
+            />
+        </div>
+    );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector("#root"));
