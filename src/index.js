f039c1e201e1ef9b8450211a5b2797ef915f1f54
Agung Sepruloh <agungsepruloh1996@gmail.com>
2019-11-01 01:12:11 +0700

    Exercise props children

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
 import ApprovalCard from "./ApprovalCard";
 import Message from "./Message";
+import Segment from "./Segment";
 
 const App = () => {
     return (
@@ -13,6 +14,25 @@ const App = () => {
             />
 
             <Message header="Attention!" messageBody="We did an update." />
+
+            <Segment>
+                <div className="ui icon header">
+                    <i className="pdf file outline icon"></i>
+                    No document are listed for this customer.
+                    <div className="ui primary button">Add document</div>
+                </div>
+            </Segment>
+
+            <Segment>
+                <h4 className="ui header">For your information</h4>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+            </Segment>
+
             <ApprovalCard>
                 <div>
                     <h4>Warning!</h4>
